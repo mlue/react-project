7af09555aaf653071bf3559e30ceebfc702454f5
Mathieu Lue <mathieu.lue@gmail.com>
2019-02-19 23:36:40 -0500

    SAVEPOINT

--- src/App.js
+++ src/App.js
@@ -3,12 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      items: [{
+        name: "name",
+        link: "link"
+      }]
+    }
+  }
+
+  onClick(){
+    this.setState({
+      items: this.state.items.concat([{
+        name: "name",
+        link: "link"
+      }])
+    })
+  }
+
+
+
   render() {
+    var listItems = this.state.items.map(function(item) {
+      return (
+        <li key="{item.name}">
+          <a href="{item.link}">{item.name}</a>
+        </li>
+      );
+    });
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <ul>
+            {listItems}
+          </ul>
+          <p className="x" onClick={() => {this.onClick()}}>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
